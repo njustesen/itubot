5eb90774fa9f47218e024090f0681ad2c27ccbd7
Niels Justesen <noju@Nielss-MacBook-Pro.local>
2017-07-13 15:53:38 +0200

    builds versus races

--- itubot/src/itubot/manager/buildorder/ScriptedBuildOrderManager.java
+++ itubot/src/itubot/manager/buildorder/ScriptedBuildOrderManager.java
@@ -13,6 +13,7 @@ import itubot.abstraction.Build;
 import itubot.abstraction.BuildType;
 import itubot.bot.ITUBot;
 import itubot.broodwar.BroodWarUnitType;
+import itubot.bwapi.Enemy;
 import itubot.bwapi.Match;
 import itubot.bwapi.Self;
 import itubot.exception.NoBuildOrderException;
@@ -45,10 +46,116 @@ public class ScriptedBuildOrderManager implements IBuildOrderManager {
 		//return pylonBuild();
 		//return nexusBuild();
 		
-		return buildOrder();
+		if (Enemy.getInstance().getRace() == Race.Terran){
+			return vsTerran();
+		} else if (Enemy.getInstance().getRace() == Race.Protoss){
+			return vsProtoss();
+		} else if (Enemy.getInstance().getRace() == Race.Zerg){
+			return vsZerg();
+		} else if (ITUBot.getInstance().informationManager.getObservations().size() > 0){
+			UnitType type = ITUBot.getInstance().informationManager.getObservations().get(0).type;
+			if (TypeRepository.protossUnits.contains(type)){
+				return vsProtoss();
+			} else if (TypeRepository.terranUnits.contains(type)){
+				return vsTerran();
+			} else if (TypeRepository.zergUnits.contains(type)){
+				return vsZerg();
+			} else {
+				return vsZerg();
+			}
+		}
+		
+		return vsZerg();
+		
+		//return buildOrder();
 	}
 	
-	private Build buildOrder() {
+	private Build vsTerran() {
+		if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 8){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Pylon) < 1){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 11){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 1){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 12){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Pylon) < 2){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 13){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 2){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (Self.getInstance().supplyUsed() + 6 > Self.getInstance().supplyTotal() && ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Pylon) < 1){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Zealot) + ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Dragoon) < ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Gateway)){
+			if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Cybernetics_Core) >= 1 && 
+					ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Assimilator) >= 1 && 
+					Self.getInstance().gas() >= 45){
+				if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Dragoon) < 1){
+					return new Build(UnitType.Protoss_Dragoon);
+				}
+			}
+			return new Build(UnitType.Protoss_Zealot);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Probe) < ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Assimilator) < 1){
+			return new Build(UnitType.Protoss_Assimilator);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Cybernetics_Core) < 1){
+			return new Build(UnitType.Protoss_Cybernetics_Core);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Zealot) > 7 && ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Probe) > 13*ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Nexus);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Robotics_Facility) < 1){
+			return new Build(UnitType.Protoss_Robotics_Facility);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCountTotal(UpgradeType.Singularity_Charge) == 0){
+			return new Build(UpgradeType.Singularity_Charge);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Observatory) < 1){
+			return new Build(UnitType.Protoss_Observatory);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Observer) < 2){
+			return new Build(UnitType.Protoss_Observer);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Forge) < 1){
+			return new Build(UnitType.Protoss_Forge);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Photon_Cannon) < 1*ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Photon_Cannon);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCountTotal(UpgradeType.Protoss_Ground_Weapons) == 0 && ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Forge) >= 1){
+			return new Build(UpgradeType.Protoss_Ground_Weapons);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 3){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Robotics_Support_Bay) < 1){
+			return new Build(UnitType.Protoss_Robotics_Support_Bay);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Reaver) < 1){
+			return new Build(UnitType.Protoss_Reaver);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Stargate) < 1){
+			return new Build(UnitType.Protoss_Stargate);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 4){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Citadel_of_Adun) < 1){
+			return new Build(UnitType.Protoss_Citadel_of_Adun);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Templar_Archives) < 1){
+			return new Build(UnitType.Protoss_Templar_Archives);
+		} else if (ITUBot.getInstance().informationManager.ownTechCountTotal(UpgradeType.Leg_Enhancements) < 1){
+			return new Build(UpgradeType.Leg_Enhancements);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Arbiter_Tribunal) < 1){
+			return new Build(UnitType.Protoss_Arbiter_Tribunal);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 5){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Arbiter) < 1){
+			return new Build(UnitType.Protoss_Arbiter);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCount(UpgradeType.Protoss_Ground_Weapons) == 1){
+			return new Build(UpgradeType.Protoss_Ground_Weapons);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCount(UpgradeType.Protoss_Ground_Armor) == 1){
+			return new Build(UpgradeType.Protoss_Ground_Armor);
+		} else if (ITUBot.getInstance().informationManager.ownTechCount(TechType.Stasis_Field) == 1){
+			return new Build(TechType.Stasis_Field);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 6){
+			return new Build(UnitType.Protoss_Gateway);
+		} else {
+			return new Build(UnitType.Protoss_Probe);
+		}
+	}
+	
+	private Build vsProtoss() {
 		if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 8){
 			return new Build(UnitType.Protoss_Probe);
 		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Pylon) < 1){
@@ -112,6 +219,8 @@ public class ScriptedBuildOrderManager implements IBuildOrderManager {
 			return new Build(UnitType.Protoss_Citadel_of_Adun);
 		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Templar_Archives) < 1){
 			return new Build(UnitType.Protoss_Templar_Archives);
+		} else if (ITUBot.getInstance().informationManager.ownTechCountTotal(UpgradeType.Leg_Enhancements) < 1){
+			return new Build(UpgradeType.Leg_Enhancements);
 		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Arbiter_Tribunal) < 1){
 			return new Build(UnitType.Protoss_Arbiter_Tribunal);
 		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 5){
@@ -131,6 +240,93 @@ public class ScriptedBuildOrderManager implements IBuildOrderManager {
 		}
 	}
 
+	private Build vsZerg() {
+		if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 8){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Pylon) < 1){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 11){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 1){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 12){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Pylon) < 2){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 13){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 2){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (Self.getInstance().supplyUsed() + 6 > Self.getInstance().supplyTotal() && ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Pylon) < 1){
+			return new Build(UnitType.Protoss_Pylon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Zealot) + ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Dragoon) < ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Gateway)){
+			if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Cybernetics_Core) >= 1 && 
+					ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Assimilator) >= 1 && 
+					Self.getInstance().gas() >= 45){
+				if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Dragoon) < 1){
+					return new Build(UnitType.Protoss_Dragoon);
+				}
+			}
+			return new Build(UnitType.Protoss_Zealot);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Probe) < ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Probe);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Assimilator) < 1){
+			return new Build(UnitType.Protoss_Assimilator);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Cybernetics_Core) < 1){
+			return new Build(UnitType.Protoss_Cybernetics_Core);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Zealot) > 3 && ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Probe) > 13*ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Nexus);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Forge) < 1){
+			return new Build(UnitType.Protoss_Forge);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Photon_Cannon) < 2*ITUBot.getInstance().informationManager.ownUnitCount(UnitType.Protoss_Nexus)){
+			return new Build(UnitType.Protoss_Photon_Cannon);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Stargate) < 1){
+			return new Build(UnitType.Protoss_Stargate);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Corsair) < 1){
+			return new Build(UnitType.Protoss_Corsair);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Robotics_Support_Bay) < 1){
+			return new Build(UnitType.Protoss_Robotics_Support_Bay);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Reaver) < 1){
+			return new Build(UnitType.Protoss_Reaver);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Robotics_Facility) < 1){
+			return new Build(UnitType.Protoss_Robotics_Facility);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 3){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCountTotal(UpgradeType.Singularity_Charge) == 0){
+			return new Build(UpgradeType.Singularity_Charge);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Observatory) < 1){
+			return new Build(UnitType.Protoss_Observatory);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Observer) < 2){
+			return new Build(UnitType.Protoss_Observer);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCountTotal(UpgradeType.Protoss_Ground_Weapons) == 0){
+			return new Build(UpgradeType.Protoss_Ground_Weapons);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 4){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Citadel_of_Adun) < 1){
+			return new Build(UnitType.Protoss_Citadel_of_Adun);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Templar_Archives) < 1){
+			return new Build(UnitType.Protoss_Templar_Archives);
+		} else if (ITUBot.getInstance().informationManager.ownTechCountTotal(UpgradeType.Leg_Enhancements) < 1){
+			return new Build(UpgradeType.Leg_Enhancements);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Arbiter_Tribunal) < 1){
+			return new Build(UnitType.Protoss_Arbiter_Tribunal);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 5){
+			return new Build(UnitType.Protoss_Gateway);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountInProd(UnitType.Protoss_Arbiter) < 1){
+			return new Build(UnitType.Protoss_Arbiter);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCount(UpgradeType.Protoss_Ground_Weapons) == 1){
+			return new Build(UpgradeType.Protoss_Ground_Weapons);
+		} else if (ITUBot.getInstance().informationManager.ownUpgradeCount(UpgradeType.Protoss_Ground_Armor) == 1){
+			return new Build(UpgradeType.Protoss_Ground_Armor);
+		} else if (ITUBot.getInstance().informationManager.ownTechCount(TechType.Stasis_Field) == 1){
+			return new Build(TechType.Stasis_Field);
+		} else if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Gateway) < 6){
+			return new Build(UnitType.Protoss_Gateway);
+		} else {
+			return new Build(UnitType.Protoss_Probe);
+		}
+	}
+	
 	private Build nexusBuild() {
 		if (ITUBot.getInstance().informationManager.ownUnitCountTotal(UnitType.Protoss_Probe) < 8){
 			return new Build(UnitType.Protoss_Probe);
